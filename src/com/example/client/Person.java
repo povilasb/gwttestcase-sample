701ad6db8eb5ee439eacfe83ad7067b0affcf5a0
Povilas Balciunas <balciunas90@gmail.com>
2014-05-23 11:11:49 +0300

    Implements Person.toJson() method.

--- src/com/example/client/Person.java
+++ src/com/example/client/Person.java
@@ -1,5 +1,9 @@
 package com.example.client;
 
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.json.client.JSONNumber;
+
 
 class Person {
 
@@ -22,5 +26,17 @@ class Person {
 
 	public int getAge() {
 		return age;
+	}
+
+	public JSONObject toJson() {
+		JSONObject result = new JSONObject();
+
+		JSONString jsonName = new JSONString(this.name);
+		result.put("name", jsonName);
+
+		JSONNumber jsonAge = new JSONNumber(this.age);
+		result.put("age", jsonAge);
+
+		return result;
 	}
 }
